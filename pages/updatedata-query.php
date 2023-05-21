dbc492ef7de10f2fa17a78f006df45bdc5b0e1af
RutulSheladiya <93831407+Rutul-Sheladiya@users.noreply.github.com>
2023-05-22 02:54:34 +0530

    fetch data into table and delete functionality working.

--- pages/updatedata-query.php
+++ pages/updatedata-query.php
@@ -1,33 +1,27 @@
 <?php
- include_once '../connection/connection.php';
- $userId = $_POST['userId'];
- $userName = $_POST['name'];
- $mobileNo = $_POST['mobileno'];
- $gender = $_POST['gender'];
- $imageName =rand().$_FILES['profile']['name'];
- $tmpname = $_FILES['profile']['tmp_name'];
- $folder = "./upload/". $imageName;
- move_uploaded_file($tmpname,$folder);
+include_once '../connection/connection.php';
+$userid = $_POST['userId'];
+$userName = $_POST['name'];  
+$mobileNo = $_POST['mobileno'];
+$gender = $_POST['gender'];
 
-if(($_FILES['profile']['name']) !== ""){
-    $sql = "UPDATE `employee` SET `name`=' $userName',`mobileno`='$mobileNo',`gender`='$gender',`image`='$imageName' WHERE id = $userId";
-    $result=mysqli_query($conn,"select image from employee where id ='$userId'");
-    $row = mysqli_fetch_assoc($result);
-    unlink("./upload/".$row['image']);
-}else{
-    $sql = "UPDATE `employee` SET `name`=' $userName',`mobileno`='$mobileNo',`gender`='$gender' WHERE id = $userId";
+
+if (($_FILES['profile']['name']) !== "") {
+    // $sql = "UPDATE `employee` SET `name`=' $userName',`mobileno`='$mobileNo',`gender`='$gender',`image`='$imageName' WHERE id = $userId";
+    // $result=mysqli_query($conn,"select image from employee where id ='$userId'");
+    // $row = mysqli_fetch_assoc($result);
+    // unlink("./upload/".$row['image']);
+    // $imageName =rand().$_FILES['profile']['name'];
+    // $tmpname = $_FILES['profile']['tmp_name'];
+    // $folder = "./upload/". $imageName;
+    // move_uploaded_file($tmpname,$folder);
+} else {
+    $sql = "UPDATE `employee` SET `name`= '$userName',`mobileno`='$mobileNo',`gender`='$gender' WHERE userid = '$userid'";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        echo json_encode(array(
+            "status" => "success",
+            "message" => "Your Record Has Been Updated."
+        ));
+    }
 }
-
-
- $result=mysqli_query($conn,$sql);
- if($result){
-    echo json_encode(array(
-        "status" => "success",
-        "message" => "Your Record Has Been Updated." 
-    ));
- }else{
-    echo json_encode(array(
-        "status" => "error",
-        "message" => "Your Record Has Been Updated." 
-    ));
- }
